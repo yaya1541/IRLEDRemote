2f0fefa7b5e6b388df13634f637e8abe7c20764d
yaya1541 <89034790+yaya1541@users.noreply.github.com>
2022-11-26 12:45:12 +0100

    add 1
    added things no need for desc

--- app/src/main/java/com/example/irledremote/MainActivity.kt
+++ app/src/main/java/com/example/irledremote/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.irledremote
 
 import android.hardware.ConsumerIrManager
-import android.media.MediaDrm.LogMessage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Debug
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -13,16 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonIds : MutableList<String> = mutableListOf();
-        val buttonColumn = 4; val buttonLine = 5;
-        val buttonNumber = 20;
+        val buttonColumn = 4; val buttonLine = 5
+        val buttonList : ArrayList<MutableList<Button>> = arrayListOf()
+        val text : TextView = findViewById(R.id.textView)
 
-        for (i in 1..buttonLine) {
-            for (j in 1..buttonColumn) {
-                buttonIds.add("button${i}_${j}")
+
+        for(i in buttonLine downTo  1){
+            buttonList.add(mutableListOf())
+            for (j in buttonColumn downTo  1){
+                val resID = resources.getIdentifier("button${i}_${j}","id",packageName)
+                buttonList[i].add(findViewById(resID))
+                buttonList[i][j].setOnClickListener { text.text = kotlin.random.Random(1).toString()}
             }
         }
-        print(buttonIds.size);
-        print(buttonNumber);
+
+
     }
 }
