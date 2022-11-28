568a6371f660ba5e48ef584314f313277fb48232
yaya1541 <yanis.niaussat@gmail.com>
2022-11-29 00:18:35 +0100

    Working LED with button
    just need to store the commands and assign them to the buttons and improve the design after

--- app/src/main/java/com/example/irledremote/MainActivity.kt
+++ app/src/main/java/com/example/irledremote/MainActivity.kt
@@ -6,38 +6,68 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.hardware.ConsumerIrManager
+import kotlin.math.pow
 
 
 /*
+Power :
 00FF 02FD -> 0000 0000 1111 1111 0000 0010 1111 1101
-
-
  */
-fun hexToBin(hex:Char):String{
+
+class binHexDec(char: CharSequence){
+    val c = char
     val hexa : ArrayList<Char> = arrayListOf('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F')
-    val bina : ArrayList<String> = arrayListOf("0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111")
-    return bina[hexa.indexOf(hex)]
+
+    fun hexToBin():String{
+
+        val bina : ArrayList<String> = arrayListOf("0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111")
+        return bina[hexa.indexOf(c[0])]
+    }
+
+    fun decToHex():String{
+        var reste: String = ""
+        var nb = c.toString().toInt()
+        while (nb / 16 != 0){
+            reste += hexa[nb % 16].toString()
+            nb = nb / 16
+        }
+        reste += hexa[nb % 16].toString()
+        return reste.reversed()
+    }
 }
+
+
 
 fun irHexComToSig(hexCommand:String): ArrayList<Int> {
     var signals = arrayListOf<Int>()
-    signals.add(9024)
-    signals.add(4512)
+
+    signals.add(9000)
+    signals.add(4500)
+
+
+
     for (l in hexCommand.asIterable()){
-        for (n in hexToBin(l)){
+        for (n in binHexDec(l as CharSequence).hexToBin()){
             if (n.toString().toInt() == 0) {
-                signals.add(564)
-                signals.add(1692)
+                signals.add(562)
+                signals.add(562)
             }else{
-                signals.add(564)
-                signals.add(564)
+                signals.add(562)
+                signals.add(1687)
             }
         }
     }
-    signals.add(12684)
+
+    signals.add(40000)
+
     return signals
 }
 
+var repeatSig = arrayListOf<Int>(9000,2250,562,96187)
+
+fun numToDeviceAndCommand(deviceNumber:Int,commandNumber: Int){
+
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,16 +91,19 @@ class MainActivity : AppCompatActivity() {
                 buttonList[i-1].add(findViewById(resID))
                 buttonList[i-1][j-1].setOnClickListener {
                     text.text = "button${i}_${j}"
-                    manager.transmit(38400, irHexComToSig("10EF906F").toIntArray())
+                    manager.transmit(38222, irHexComToSig("00FF02FD").toIntArray())
+                }
+                buttonList[i-1][j-1].setOnLongClickListener {
+                    buttonList[i-1][j-1].isPressed = false
+                    manager.transmit(38222, irHexComToSig("00FF02FD").toIntArray())
+                    manager.transmit(38222, repeatSig.toIntArray())
+                    return@setOnLongClickListener true
                 }
             }
         }
-        if (manager.hasIrEmitter()){
-            text.text = irHexComToSig("10EF906F").toString()
-            /*manager.carrierFrequencies[5].maxFrequency.toString()
-            manager.transmit(38000)*/
-        }
-        /*text.text = k*/
+
+
+        text.text = binHexDec("44" as CharSequence).decToHex()
 
     }
 }
